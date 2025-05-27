77dbef463b996462fba317f375bc3a19970f670d
Eugine koyo <euginekoyo003@gmail.com>
2025-05-27 10:44:12 +0300

    basic connection to postgres database

---
+++ src/main/webapp/app/entities/employee/employee-detail.tsx
@@ -0,0 +1,107 @@
+import React, { useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { Button, Col, Row, UncontrolledTooltip } from 'reactstrap';
+import { TextFormat, Translate } from 'react-jhipster';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import { APP_DATE_FORMAT } from 'app/config/constants';
+import { useAppDispatch, useAppSelector } from 'app/config/store';
+
+import { getEntity } from './employee.reducer';
+
+export const EmployeeDetail = () => {
+  const dispatch = useAppDispatch();
+
+  const { id } = useParams<'id'>();
+
+  useEffect(() => {
+    dispatch(getEntity(id));
+  }, []);
+
+  const employeeEntity = useAppSelector(state => state.employee.entity);
+  return (
+    <Row>
+      <Col md="8">
+        <h2 data-cy="employeeDetailsHeading">
+          <Translate contentKey="appApp.employee.detail.title">Employee</Translate>
+        </h2>
+        <dl className="jh-entity-details">
+          <dt>
+            <span id="id">
+              <Translate contentKey="global.field.id">ID</Translate>
+            </span>
+          </dt>
+          <dd>{employeeEntity.id}</dd>
+          <dt>
+            <span id="firstName">
+              <Translate contentKey="appApp.employee.firstName">First Name</Translate>
+            </span>
+            <UncontrolledTooltip target="firstName">
+              <Translate contentKey="appApp.employee.help.firstName" />
+            </UncontrolledTooltip>
+          </dt>
+          <dd>{employeeEntity.firstName}</dd>
+          <dt>
+            <span id="lastName">
+              <Translate contentKey="appApp.employee.lastName">Last Name</Translate>
+            </span>
+          </dt>
+          <dd>{employeeEntity.lastName}</dd>
+          <dt>
+            <span id="email">
+              <Translate contentKey="appApp.employee.email">Email</Translate>
+            </span>
+          </dt>
+          <dd>{employeeEntity.email}</dd>
+          <dt>
+            <span id="phoneNumber">
+              <Translate contentKey="appApp.employee.phoneNumber">Phone Number</Translate>
+            </span>
+          </dt>
+          <dd>{employeeEntity.phoneNumber}</dd>
+          <dt>
+            <span id="hireDate">
+              <Translate contentKey="appApp.employee.hireDate">Hire Date</Translate>
+            </span>
+          </dt>
+          <dd>{employeeEntity.hireDate ? <TextFormat value={employeeEntity.hireDate} type="date" format={APP_DATE_FORMAT} /> : null}</dd>
+          <dt>
+            <span id="salary">
+              <Translate contentKey="appApp.employee.salary">Salary</Translate>
+            </span>
+          </dt>
+          <dd>{employeeEntity.salary}</dd>
+          <dt>
+            <span id="commissionPct">
+              <Translate contentKey="appApp.employee.commissionPct">Commission Pct</Translate>
+            </span>
+          </dt>
+          <dd>{employeeEntity.commissionPct}</dd>
+          <dt>
+            <Translate contentKey="appApp.employee.manager">Manager</Translate>
+          </dt>
+          <dd>{employeeEntity.manager ? employeeEntity.manager.id : ''}</dd>
+          <dt>
+            <Translate contentKey="appApp.employee.department">Department</Translate>
+          </dt>
+          <dd>{employeeEntity.department ? employeeEntity.department.id : ''}</dd>
+        </dl>
+        <Button tag={Link} to="/employee" replace color="info" data-cy="entityDetailsBackButton">
+          <FontAwesomeIcon icon="arrow-left" />{' '}
+          <span className="d-none d-md-inline">
+            <Translate contentKey="entity.action.back">Back</Translate>
+          </span>
+        </Button>
+        &nbsp;
+        <Button tag={Link} to={`/employee/${employeeEntity.id}/edit`} replace color="primary">
+          <FontAwesomeIcon icon="pencil-alt" />{' '}
+          <span className="d-none d-md-inline">
+            <Translate contentKey="entity.action.edit">Edit</Translate>
+          </span>
+        </Button>
+      </Col>
+    </Row>
+  );
+};
+
+export default EmployeeDetail;
